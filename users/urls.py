0b817798f2828b810bf096b64b1706f6d49a43e0
wawiorka <anya.shaman@gmail.com>
2025-07-31 21:20:12 +0300

    add api-auth in DRF

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from .views import RegisterView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('sign-up/', RegisterView.as_view()),
     path('sign-in/', TokenObtainPairView.as_view()),
-    path('refresh/', TokenRefreshView.as_view())
+    path('refresh/', TokenRefreshView.as_view()),
+    path('api-auth/', include('rest_framework.urls')),
     ]
